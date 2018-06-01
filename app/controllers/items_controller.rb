2b6c882d96a6075c95fa17182f67790a924b8b0d
yasuhiro.sakano <yasuhiro7499@gmail.com>
2018-06-01 22:57:05 +0000

    want

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
     @items = []
 
     @keyword = params[:keyword]
-    if @keyword.present? # 
+    if @keyword  
       results = RakutenWebService::Ichiba::Item.search({
         keyword: @keyword,
         imageFlag: 1,
@@ -13,25 +13,19 @@ class ItemsController < ApplicationController
       })
 
       results.each do |result|
-        item = Item.new(read(result))
+        item = Item.find_or_initialize_by(read(result))
         @items << item
       end
     end
   end
+  
+  
+  def show
+    @item = Item.find(params[:id])
+    @want_user = @item.want_users
+  end
 
   private
 
-  def read(result)
-    code = result['itemCode']
-    name = result['itemName']
-    url = result['itemUrl']
-    image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
-
-    return {
-      code: code,
-      name: name,
-      url: url,
-      image_url: image_url,
-    }
-  end
+  
 end
