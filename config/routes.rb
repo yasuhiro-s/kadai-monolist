877eab92f9b0eb31f81fffa0af70b13fa488ef57
yasuhiro.sakano <yasuhiro7499@gmail.com>
2018-06-02 09:13:30 +0000

    for heroku rankings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
-
+  
+  get 'rankings/want', to: 'rankings#want'
+  get 'rankings/have', to: 'rankings#have'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
